a07c27654d3f0767e57d4ed629a6e2aa936d64a3
efratkitov <ekt82577@gmail.com>
2025-01-14 00:59:45 +0200

    הוספת המשתנה x

--- DisplayDate/DisplayDate/MainWindow.xaml.cs
+++ DisplayDate/DisplayDate/MainWindow.xaml.cs
@@ -29,8 +29,7 @@ namespace DisplayDate
 
             // הצגת התאריך והשעה בתוך ה-Label
             datalable.Content = $"Current Date and Time: {formattedDateTime}";
-
-
+            int x = 5;
         }
     }
 }
